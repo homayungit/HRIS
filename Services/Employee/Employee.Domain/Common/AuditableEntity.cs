b45bf61a1273648aa61518018be633c74e2dcb28
homayungit <homayunjust@gmail.com>
2024-03-14 15:17:46 +0600

    Add Auditable entity

---
+++ Services/Employee/Employee.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Employee.Domain.Common
+{
+	public abstract class AuditableEntity
+	{
+        public DateTime CreatedAt { get; set; }
+		public string CreatedBy { get; set;}= string.Empty;
+		public DateTime UpdatedAt { get; set; }
+		public string UpdatedBy { get; set; } = string.Empty;
+	}
+}
